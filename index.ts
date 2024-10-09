cc7d04da0e4bd78678aa8806fafd211abc010bce
Ableez <djayableez@gmail.com>
2024-10-09 19:52:07 +0100

    cors fix for frontend routes

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-// File: index.ts
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { cors } from "@elysiajs/cors";
 import { contentsRouter } from "./routers/content";
-import webhookRouter from "./routers/webhookController";
 import { usersRouter } from "./routers/user";
 
 const app = new Elysia()
   .use(swagger())
-  .use(cors())
+  .use(
+    cors({
+      origin: ["http://localhost:3000", "https://morse-web3.vercel.app"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  )
   .group("/api", (app) => app.use(contentsRouter).use(usersRouter))
   .listen(process.env.BACK_PORT || 3030);
 
